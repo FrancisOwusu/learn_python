13306946f8e3fa68683f42ea93484b5289bd6580
francis.owusu <francis.owusu@wemakeit.nu>
2023-06-01 18:32:52 +0000

    Init commit

---
+++ ch1/prefix.py
@@ -0,0 +1,10 @@
+filename = "https://nostarch.com"
+newFileName =filename.removesuffix("")
+
+
+
+nostarch_url = 'https://nostarch.com'
+newUrl=nostarch_url.removeprefix("https://")
+print(newUrl)
+#
+#nostarch_url.removeprefix('https://')
---
+++ ch1/printName.py
@@ -0,0 +1,9 @@
+person_name = "Francis Osei Owusu"
+
+print(f"Hello {person_name}, would you like to learn some Python today?")
+
+#print name in lower,upper and title case
+print(f"Hello {person_name.upper()}, would you like to learn some Python today?")
+print(f"Hello {person_name.lower()}, would you like to learn some Python today?")
+print(f"Hello {person_name.title()}, would you like to learn some Python today?")
+
