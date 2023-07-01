e3339766b06820a3e895d60123025dc68a2a3327
francis.owusu <francis.owusu@wemakeit.nu>
2023-07-01 19:07:56 +0000

    update on conditions

---
+++ ch5/magic_number.py
@@ -0,0 +1,9 @@
+answer = 17
+if answer != 42:
+ print("That is not the correct answer. Please try again!")
+ 
+ requested_toppings = ['mushrooms', 'onions', 'pineapple']
+ 'mushrooms' in requested_toppings
+print(True)
+'pepperoni' in requested_toppings
+# print(True)
