92f30b78311493b6888f1d15f0065b7e1dac2973
FrancisOwusu <osei1257@gmail.com>
2024-11-26 21:11:06 +0000

    add modules

--- ch9/car.py
+++ ch9/car.py
@@ -1,3 +1,6 @@
+from battery import Battery
+
+
 class Car:
     def __init__(self, make, model, year):
         """Initialize attributes to describe a car."""
@@ -19,13 +22,41 @@ class Car:
         """Set the odometer reading to the given value.
         Reject the change if it attempts to roll the odometer back"""
         if mileage >= self.odometer_reading:
-            self.odometer_reading =mileage
+            self.odometer_reading = mileage
         else:
             print("You can't roll back on odometer!")
         # self.odometer_reading = mileage
-    def increment_odometer(self,miles):
+
+    def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
-        self.odometer_reading +=miles
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car,specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 40
+        self.battery = Battery()
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    # method overriding
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't have a gas tank!")
+
+
+my_leaf = ElectricCar("nissan", "leaf", 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
+
+my_leaf.battery.battery_size()
+my_leaf.battery.get_range()
+
 
 my_new_car = Car("audi", "a4", 2024)
 print(my_new_car.get_descriptive_name())
@@ -35,7 +66,7 @@ my_new_car.odometer_reading = 56
 my_new_car.read_odometer()
 
 
-my_used_car = Car('subaru', 'outback', 2019)
+my_used_car = Car("subaru", "outback", 2019)
 print(my_used_car.get_descriptive_name())
 
 my_used_car.update_odometer(23_500)
---
+++ ch9/my_electric_car.py
@@ -0,0 +1,8 @@
+from car import Car,ElectricCar
+
+my_mustag = Car("ford","mustang","2024")
+my_leaf = ElectricCar("nissan", "leaf", 2024)
+print(my_mustag.get_descriptive_name())
+my_leaf=ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
+#my_leaf.battery.get_range()
