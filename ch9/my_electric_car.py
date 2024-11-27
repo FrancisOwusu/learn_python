75cd0c745e63a261961bb3627a9f6172ea55b8b6
FrancisOwusu <osei1257@gmail.com>
2024-11-27 11:07:07 +0000

    complete ch9

--- ch9/my_electric_car.py
+++ ch9/my_electric_car.py
@@ -1,8 +1,8 @@
-from car import Car,ElectricCar
-
-my_mustag = Car("ford","mustang","2024")
-my_leaf = ElectricCar("nissan", "leaf", 2024)
-print(my_mustag.get_descriptive_name())
-my_leaf=ElectricCar('nissan','leaf',2024)
+from car import ElectricCar
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
-#my_leaf.battery.get_range()
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
+
+
+
