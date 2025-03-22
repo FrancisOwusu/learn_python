43bbbc68cfec4586d051f6b557d9018014ee56a7
FrancisOwusu <osei1257@gmail.com>
2025-03-22 12:02:01 +0000

    git unit test is complete

---
+++ unit_testing/mymodule.py
@@ -0,0 +1,13 @@
+"""Module providing a function function square and double."""
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number**2
+
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
