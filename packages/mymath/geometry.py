e6b8d5392bfeda68d15091761899f79cc8219f91
FrancisOwusu <osei1257@gmail.com>
2025-03-24 13:13:05 +0000

    packe develoment complete

---
+++ packages/mymath/geometry.py
@@ -0,0 +1,19 @@
+"""Geometry module"""
+
+import math
+
+
+def area_of_rectangle(length, breath):
+    """_summary_
+
+    Args:
+        length (_type_): _description_
+        breath (_type_): _description_
+    """
+
+    return length * breath
+
+
+def area_of_circle(radius):
+    """return the area of circle"""
+    return math.pi * radius * radius
